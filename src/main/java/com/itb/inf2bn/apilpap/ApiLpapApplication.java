aaab6fff6399809d668702914871af03dd3b4d52
Matheus Brum Costa Lima <rm91838@estudante.fieb.edu.br>
2024-09-24 18:48:16 -0300

    Inicio Contrle Deversão

---
+++ src/main/java/com/itb/inf2bn/apilpap/ApiLpapApplication.java
@@ -0,0 +1,13 @@
+package com.itb.inf2bn.apilpap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLpapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLpapApplication.class, args);
+	}
+
+}
