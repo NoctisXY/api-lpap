424f8435bec2c5fce6f8fa4ec8b8f14324f54df8
Matheus Brum Costa Lima <rm91838@estudante.fieb.edu.br>
2024-10-01 18:54:59 -0300

    Início Controle de versão

--- src/main/java/com/itb/inf2bn/apilpap/ApiLpapApplication.java
+++ src/main/java/com/itb/inf2bn/apilpap/ApiLpapApplication.java
@@ -8,6 +8,19 @@ public class ApiLpapApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiLpapApplication.class, args);
+
+		/* Com o projeto no Github...*/
+
+		/*
+		* Cada atualização no repositório local, devemos prosseguir com os seguintes passos:
+		* 1° git add
+		* 2° git commit -m "mensagem correspondente do que foi feito"
+		* 3º git push
+		 */
+
+		System.out.println("Primeira atualização após o github");
+
+
 	}
 
 }
